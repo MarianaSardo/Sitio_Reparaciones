e7ab10c59e1255d9d13bbc6fb36cd838d9866f36
Mariana Sardo <103539258+MarianaSardo@users.noreply.github.com>
2023-03-18 17:52:00 -0300

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ include 'header.php';
         <label for="nombre">Nombre cliente:</label>
         <input type="text" name="nombre" id="nombre"  placeholder="Nombre Apellido" title="Por favor, solo ingrese nombre y apellido" pattern="[a-zA-Z\s]+" minlength="4" maxlength="100" required><br>
         <label for="telefono">Telefono:</label>
-        <input type="text" name="telefono" id="telefono"  placeholder="4751111" title="Por favor, solo ingrese n&uacute;meros" minlength="5" maxlength="12"  pattern="[0-9]+" required>
+        <input type="number" name="telefono" id="telefono"  placeholder="4751111" min="99999"  max="9999999" required>
         <label for="mail">Email:</label>
         <input type="email" name="mail" id="mail"  placeholder="usuario@mail.com"  pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required>
         <h3>Datos del producto</h3>
@@ -26,7 +26,7 @@ include 'header.php';
         <label for="marca">Marca:</label>
         <input type="text" name="marca" id="marca"  placeholder="Marca"  title="Por favor,ingrese solo la marca" pattern="[a-zA-Z\s]+" minlength="3" maxlength="100" required><br>
         <label for="numSerie">Numero de serie:</label>
-        <input type="text" name="numSerie" id="numSerie"  title="Por favor, solo ingrese n&uacute;meros" placeholder="123" pattern="[0-9]+" required>
+        <input type="number" name="numSerie" id="numSerie"  placeholder="123" min="0" required>
         <label for="modelo">Modelo:</label>
         <input type="text" name="modelo" id="modelo"  placeholder="abc123" required><br>
         <label for="problema">Motivo de ingreso:</label>
