fa645e7aef7666671e49177eb949d8e28c5d1173
Mariana Sardo <103539258+MarianaSardo@users.noreply.github.com>
2023-03-18 15:12:12 -0300

    Update guardar_datos.php

--- guardar_datos.php
+++ guardar_datos.php
@@ -17,7 +17,7 @@ $repuesto = "Sin definir";
 $costoRepuesto = 0;
 $costoTotal = 0;
 $estado = 1;
-$fechaEntrega = "00/00/0000";
+$fechaEntrega = NULL;
 
 
 $sql = "INSERT INTO reparar (fecha,nombre,telefono,mail,producto,marca,numSerie,modelo,problema,diagnostico,repuesto,costoRepuesto,costoTotal,estado,fechaEntrega) VALUES ('$fecha','$nombre','$telefono','$mail','$producto','$marca','$numSerie','$modelo','$problema','$diagnostico','$repuesto','$costoRepuesto','$costoTotal','$estado','$fechaEntrega')";
